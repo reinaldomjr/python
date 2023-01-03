e98a96e4d4205daf573c6fdd1ef4e818309a806e
Reinaldo Oliveira Machado Jr <reinaldo.m.jr@gmail.com>
2023-01-02 22:01:49 -0300

    qaweasdqaweasd

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('asdasdfsadf')
+    print_hi('123123123123')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
